e4b56f2f68225846554b22b341e22528fd4a4880
Hagen Klemp <it-service@softleister.de>
2017-08-06 15:24:24 +0200

    Feldgröße

--- src/Resources/contao/dca/tl_form_field.php
+++ src/Resources/contao/dca/tl_form_field.php
@@ -31,6 +31,6 @@ $GLOBALS['TL_DCA']['tl_form_field']['fields']['headline'] = array
     'search'                  => true,
     'inputType'               => 'inputUnit',
     'options'                 => array('h1', 'h2', 'h3', 'h4', 'h5', 'h6'),
-    'eval'                    => array('maxlength'=>255, 'tl_class'=>'w50 clr'),
-    'sql'                     => "varchar(8) NOT NULL default ''"
+    'eval'                    => array('maxlength'=>200, 'tl_class'=>'w50 clr'),
+    'sql'                     => "varchar(255) NOT NULL default ''"
 );
